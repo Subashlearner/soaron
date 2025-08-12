2649de2f83fe153758674c84655a7ae5a7a93cd8
Subashlearner <subash.2003.21.10@gmail.com>
2025-08-12 14:50:26 +0530

    Initial commit

---
+++ soaronweb/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ComingSoon from "./ComingSoon";
+
+function App() {
+  return (
+    <React.StrictMode>
+      <ComingSoon />
+    </React.StrictMode>
+  );
+}
+
+export default App;
---
+++ soaronweb/src/ComingSoon.js
@@ -0,0 +1,69 @@
+import React from "react";
+import "./ComingSoon.css";
+import { FaYoutube, FaLinkedin, FaInstagram } from "react-icons/fa";
+import { RxCross2 } from "react-icons/rx";
+
+export default function ComingSoon() {
+  return (
+    <div className="comingsoon-container">
+      {/* Header */}
+      <header className="header">
+        <h1 className="logo">
+          soar<span className="logo-highlight">on</span>
+        </h1>
+        <button className="menu-btn">
+          Menu <span className="menu-icon">☰</span>
+        </button>
+      </header>
+
+      {/* Main Content */}
+      <main className="main-content">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/3940/3940056.png"
+          alt="Rocket"
+          className="rocket-img"
+        />
+        <h2 className="main-title">WE ARE COMING SOON!!</h2>
+        <p className="subtitle">Stay tuned for something amazing</p>
+      </main>
+
+      {/* Footer */}
+     <footer className="footer">
+  {/* Top Row */}
+  <div className="footer-top">
+    <div className="footer-links">
+      <a href="#">Home</a>
+      <a href="#">Story</a>
+      <a href="#">Career</a>
+      <a href="#">Partner With Us</a>
+      <a href="#">Contact Us</a>
+      <a href="#">Patent & Trademark</a>
+    </div>
+    <div className="social-icons">
+      <FaYoutube />
+      <FaLinkedin />
+      <RxCross2 />
+      <FaInstagram />
+    </div>
+  </div>
+
+  {/* Bottom Row */}
+  <div className="footer-bottom">
+    <div className="small-links">
+      <a href="#">Privacy Policy</a>
+      <span>•</span>
+      <a href="#">Terms & Conditions</a>
+      <span>•</span>
+      <a href="#">Refund Policy</a>
+      <span>•</span>
+      <a href="#">Press or Media</a>
+      <span>•</span>
+      <a href="#">Help & Support</a>
+    </div>
+    <p className="copyright">Copyright © Soaron All Right Reserved</p>
+  </div>
+</footer>
+
+    </div>
+  );
+}
