b4fdf2a750061fa470b6d3d5281617d6a3711e47
Subashlearner <subash.2003.21.10@gmail.com>
2025-08-13 14:36:56 +0530

    update6

--- soaronweb/src/ComingSoon.js
+++ soaronweb/src/ComingSoon.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./ComingSoon.css";
-import { FaYoutube, FaLinkedin, FaInstagram } from "react-icons/fa";
-import { RxCross2 } from "react-icons/rx";
+import { FaYoutube, FaLinkedinIn, FaInstagram } from "react-icons/fa";
 
 export default function ComingSoon() {
   return (
@@ -45,6 +44,7 @@ export default function ComingSoon() {
               target="_blank"
               rel="noopener noreferrer"
               aria-label="YouTube"
+              className="icon youtube-icon"
             >
               <FaYoutube />
             </a>
@@ -53,24 +53,39 @@ export default function ComingSoon() {
               target="_blank"
               rel="noopener noreferrer"
               aria-label="LinkedIn"
+              className="icon linkedin-icon"
             >
-              <FaLinkedin />
+              <FaLinkedinIn />
             </a>
-            <button
-              type="button"
-              className="icon-button"
-              aria-label="Close"
+            <a
+              href="https://twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Twitter"
+              className="icon twitter-icon"
             >
-              <RxCross2 />
-            </button>
+              {/* Inline SVG for X logo */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 1200 1227"
+                fill="currentColor"
+                width="11"
+                height="11"
+
+              >
+                <path d="M714.163 519.284L1160.89 0H1056.1L668.74 450.887L388.508 0H0l466.977 729.032L0 1227h104.782l407.182-478.552L811.492 1227h388.508L714.163 519.284zM562.189 674.546l-47.51-72.34L142.343 91.421h181.239l305.081 464.988l47.51 72.34l389.828 592.661H884.762L562.189 674.546z" />
+              </svg>
+            </a>
             <a
               href="https://www.instagram.com"
               target="_blank"
               rel="noopener noreferrer"
               aria-label="Instagram"
+              className="icon instagram-icon"
             >
               <FaInstagram />
             </a>
+
           </div>
         </div>
 
